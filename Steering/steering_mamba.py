476789d3fb229219d143a5c1ed7c3480b9205f94
Nora Belrose <belrose.nora@gmail.com>
2024-03-08 08:15:53 +0000

    Refactor & speedup

--- Steering/steering_mamba.py
+++ Steering/steering_mamba.py
@@ -1,40 +1,44 @@
 from argparse import ArgumentParser
+from dataclasses import asdict, dataclass, field
+from pathlib import Path
 import json
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import torch
-from torch.nn import functional as F
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
 
+@dataclass
 class ActivationAdder:
-    def __init__(self):
-        self.activations = []
+    positives: list = field(default_factory=list)
+    negatives: list = field(default_factory=list)
 
-    def record_activations(self):
+    mamba_style: bool = False
+
+    def record_activations(self, positive: bool = True):
         def hook(model, input, output):
-            act = output[0] + output[1]
-            self.activations.append(act.detach()[:, -2:, :].cpu())
+            act = output[0] + output[1] if self.mamba_style else output[0]
+            act = act.detach()[:, -2, :].cpu()
+            if positive:
+                self.positives.append(act)
+            else:
+                self.negatives.append(act)
 
         return hook
 
     def add_activations(self, act=None, mult=1, start_pos=-1):
         if act is None:
-            act = sum(self.activations) / len(self.activations)
+            pos_mean = sum(self.positives) / len(self.positives)
+            neg_mean = sum(self.negatives) / len(self.negatives)
+            act = pos_mean - neg_mean
+            act /= act.norm()
 
         def hook(model, input, output):
-            output[0][:, start_pos:, :] += mult * act.cuda(device)
+            output[0][:, start_pos, :] += mult * act.to(output[0].device)
             return output
 
         return hook
-
-    def save_activations(self, path):
-        torch.save(self.activations, path)
-
-    def load_activations(self, path):
-        self.activations = torch.load(path)
 
 
 def get_layer_list(model: torch.nn.Module) -> torch.nn.ModuleList:
@@ -64,18 +68,39 @@ def get_layer_list(model: torch.nn.Module) -> torch.nn.ModuleList:
     )
 
 
+TEMPLATES = {
+    "hermes": "\x16user\n{}\x17\n\x16assistant\n(",
+    "llama": "[INST] {} [/INST] (",
+}
+
+
 if __name__ == "__main__":
+    dataset_root = Path(__file__).parent.parent / "datasets" / "generate"
+
     parser = ArgumentParser()
+    parser.add_argument(
+        "behavior",
+        type=str,
+        choices=[folder.stem for folder in dataset_root.iterdir()],
+    )
     parser.add_argument("--device", type=str, default="cuda:2")
+    parser.add_argument(
+        "--template", type=str, choices=("hermes", "llama"), default="hermes"
+    )
     parser.add_argument(
         "--model", type=str, default="EleutherAI/Hermes-mamba-2.8b-slimpj-cDPO"
     )
     args = parser.parse_args()
 
+    template = TEMPLATES[args.template]
+    print(f"Using template:\n{template}")
+
     # Kind of a hack, maybe fix later
     if "mamba" in args.model.lower():
         from mamba_ssm.models.config_mamba import MambaConfig
         from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+
+        # from mamba_ssm.utils.generation import InferenceParams
         from mamba_ssm.utils.hf import load_config_hf, load_state_dict_hf
 
         config_data = load_config_hf(args.model)
@@ -97,241 +122,125 @@ if __name__ == "__main__":
         model = AutoModelForCausalLM.from_pretrained(args.model).to(args.device)
         tokenizer = AutoTokenizer.from_pretrained(args.model)
 
-    device = "cuda:2"
-
-    a_token_id = tokenizer.encode("A")
-    b_token_id = tokenizer.encode("B")
+    a_token_id = tokenizer.convert_tokens_to_ids("A")
+    b_token_id = tokenizer.convert_tokens_to_ids("B")
 
     layer_list = get_layer_list(model)
-    layer_indices = list(range(0, 63, 2)) + [63]
 
-    dataset_path = "../datasets/generate/survival-instinct/generate_dataset.json"
-    with open(dataset_path, "rb") as fd:
-        dataset = json.load(fd)
+    act_root = Path("activations") / args.model
+    if act_root.exists():
+        print("Loading cached activations...")
+        actadds = [
+            ActivationAdder(**torch.load(act_root / f"{args.behavior}_{layer}.pt"))
+            for layer in range(len(layer_list))
+        ]
+    # Create all the activations
+    else:
+        print("Generating activations...")
 
-    actadds = {layer: ActivationAdder() for layer in layer_indices}
-    for prompt in tqdm(dataset, desc="Generating steering vector"):
-        msg = prompt["question"]
-        a_matches = prompt["answer_matching_behavior"] == "(A)"
-        msg = "\x16user\n" + msg + "\x17\n\x16assistant\n"
+        dataset_path = dataset_root / args.behavior / "generate_dataset.json"
+        with open(dataset_path, "rb") as fd:
+            dataset = json.load(fd)
 
-        with torch.no_grad():
-            positive_prompt = msg + "(A)" if a_matches else msg + "(B)"
-            pos_activations = [
-                model.backbone.layers[layer].register_forward_hook(
-                    actadds[layer].record_activations()
+        actadds = [
+            ActivationAdder(mamba_style="mamba" in args.model.lower())
+            for _ in range(len(layer_list))
+        ]
+        for prompt in tqdm(dataset, desc="Generating steering vector"):
+            msg = prompt["question"]
+            a_matches = prompt["answer_matching_behavior"] == "(A)"
+            msg = template.format(msg)
+
+            with torch.autocast("cuda", dtype=torch.bfloat16), torch.no_grad():
+                positive_prompt = msg + "A)" if a_matches else msg + "B)"
+                pos_activations = [
+                    layer.register_forward_hook(adder.record_activations(positive=True))
+                    for adder, layer in zip(actadds, layer_list)
+                ]
+                model(
+                    torch.tensor(tokenizer.encode(positive_prompt))
+                    .unsqueeze(0)
+                    .to(args.device)
                 )
-                for layer in layer_indices
-            ]
-            model.forward(
-                torch.tensor(tokenizer.encode(positive_prompt))
-                .unsqueeze(0)
-                .cuda(device)
-            )
-            for h in pos_activations:
-                h.remove()
+                for h in pos_activations:
+                    h.remove()
 
-            neg_activations = [
-                model.backbone.layers[layer].register_forward_hook(
-                    actadds[layer].record_activations()
+                neg_activations = [
+                    layer.register_forward_hook(
+                        adder.record_activations(positive=False)
+                    )
+                    for adder, layer in zip(actadds, layer_list)
+                ]
+                negative_prompt = msg + "B)" if a_matches else msg + "A)"
+                model(
+                    torch.tensor(tokenizer.encode(negative_prompt))
+                    .unsqueeze(0)
+                    .to(args.device)
                 )
-                for layer in layer_indices
-            ]
-            negative_prompt = msg + "(B)" if a_matches else msg + "(A)"
-            model.forward(
-                torch.tensor(tokenizer.encode(negative_prompt))
-                .unsqueeze(0)
-                .cuda(device)
-            )
-            for h in neg_activations:
-                h.remove()
+                for h in neg_activations:
+                    h.remove()
 
-    for layer in layer_indices:
-        actadds[layer].save_activations(f"slimpj-dpo-survival-instinct_{layer}.pt")
+        for layer in range(len(layer_list)):
+            path = act_root / f"{args.behavior}_{layer}.pt"
+            path.parent.mkdir(parents=True, exist_ok=True)
 
-    test_dataset_path = "../datasets/test/survival-instinct/test_dataset_ab.json"
+            torch.save(asdict(actadds[layer]), path)
+
+    test_dataset_path = (
+        dataset_root.parent / f"test/{args.behavior}/test_dataset_ab.json"
+    )
     with open(test_dataset_path, "rb") as fd:
         test_dataset = json.load(fd)
 
-    for layer in layer_indices:
-        actadds[layer].load_activations(f"slimpj-dpo-survival-instinct_{layer}.pt")
-    results = []
-    for layer in layer_indices:
-        for mult in [-1, -0.5, -0.1, 0, 0.1, 0.5, 1]:
-            average_prob = 0
-            count = 0
+    # Rows in a dataframe
+    records: list[dict] = []
 
-            module = model.backbone.layers[layer]
-            matching_prob = 0
-            not_matching_prob = 0
-            correct = 0
-            h = module.register_forward_hook(
-                actadds[layer].add_activations(mult=mult, start_pos=-1)
-            )
-            # for prompt in tqdm(test_dataset, desc="Processing prompts"):
-            for prompt in test_dataset:
-                count += 1
-                msg = prompt["question"]
-                a_matches = prompt["answer_matching_behavior"] == "(A)"
-                msg = "\x16user\n" + msg + "\x17\n\x16assistant\n("
-                with torch.no_grad():
-                    out = model.forward(
-                        torch.tensor(tokenizer.encode(msg)).unsqueeze(0).cuda(device)
+    for prompt in tqdm(test_dataset):
+        msg = prompt["question"]
+        a_matches = prompt["answer_matching_behavior"] == "(A)"
+        msg = template.format(msg)
+
+        inputs = torch.tensor(tokenizer.encode(msg)).unsqueeze(0).to(args.device)
+
+        with torch.autocast("cuda", dtype=torch.bfloat16), torch.no_grad():
+            # We're only applying the steering vector to the last token
+            prefix, suffix = inputs[:, :-1], inputs[:, -1:]
+
+            # Pre-compute the KV cache or state for the prefix
+            kv_cache = model(prefix, use_cache=True).past_key_values
+
+            for i, layer in enumerate(get_layer_list(model)):
+                for mult in [-1, -0.5, -0.1, 0, 0.1, 0.5, 1]:
+                    # Add the appropriate forward hook
+                    h = layer.register_forward_hook(
+                        actadds[i].add_activations(mult=mult, start_pos=-1)
+                    )
+                    logits = model(suffix, past_key_values=kv_cache).logits[0, -1, :]
+
+                    # Make sure to remove it!
+                    h.remove()
+
+                    probs = logits.softmax(-1)
+                    a_prob = probs[a_token_id].item()
+                    b_prob = probs[b_token_id].item()
+
+                    matching_prob = (a_prob if a_matches else b_prob) / (
+                        a_prob + b_prob
+                    )
+                    nonsense_prob = 1 - (a_prob + b_prob)
+
+                    records.append(
+                        {
+                            "layer": i,
+                            "multiplier": mult,
+                            "matching": matching_prob,
+                            "nonsense": nonsense_prob,
+                        }
                     )
 
-                logits = out.logits[0, -1, :]
-                probs = F.softmax(logits.float(), dim=-1).cpu().numpy()
-                a_prob = probs[a_token_id]
-                b_prob = probs[b_token_id]
+    df = pd.DataFrame.from_records(records)
+    stats = df.groupby(["layer", "multiplier"]).mean()
 
-                max_prob = F.softmax(logits.float(), dim=-1).argmax().cpu().numpy()
-                if max_prob == a_token_id and a_matches:
-                    correct += 1
-                if max_prob == b_token_id and not a_matches:
-                    correct += 1
-
-                behavior_prob = a_prob if a_matches else b_prob
-                average_prob += behavior_prob / (a_prob + b_prob)
-                matching_prob += behavior_prob
-                not_matching_prob += a_prob if not a_matches else b_prob
-            results.append(
-                (
-                    layer,
-                    mult,
-                    -1,
-                    average_prob / count,
-                    matching_prob / count,
-                    not_matching_prob / count,
-                    correct / count,
-                )
-            )
-            h.remove()
-            print(
-                f"{layer}, {mult}, {average_prob / count}, {matching_prob / count},"
-                f" {not_matching_prob / count}, {correct/count}"
-            )
-
-    cleandata = []
-    colors = [
-        "red",
-        "blue",
-        "green",
-        "yellow",
-        "purple",
-        "orange",
-        "black",
-        "pink",
-        "brown",
-    ]
-    for i, values in enumerate(results):
-        cleandata.append(
-            [
-                values[0],
-                values[1],
-                values[2],
-                values[3][0],
-                values[4][0],
-                values[5][0],
-                values[6],
-            ]
-        )
-
-    dataframe = pd.DataFrame(
-        cleandata,
-        columns=[
-            "Layer",
-            "Mult",
-            "Pos",
-            "Average",
-            "Matching",
-            "Not Matching",
-            "Correct",
-        ],
-    )
-    dataframe.to_csv("slimpj-dpo-survival-instincts.csv")
-
-    mults = dataframe["Mult"].unique()
-    for i, m in enumerate(mults):
-        data_previous = dataframe[dataframe["Pos"] == -1]
-
-        data_previous = data_previous[data_previous["Mult"] == m]
-        plt.plot(
-            data_previous["Layer"],
-            data_previous["Matching"]
-            / (data_previous["Matching"] + data_previous["Not Matching"]),
-            color=colors[i],
-            label=f"Mult: {m}",
-        )
-    plt.legend()
-    plt.figure()
-    mults = dataframe["Mult"].unique()
-    for i, m in enumerate(mults):
-        data_previous = dataframe[dataframe["Pos"] == -1]
-
-        data_previous = data_previous[data_previous["Mult"] == m]
-        plt.plot(
-            data_previous["Layer"],
-            data_previous["Matching"],
-            color=colors[i],
-            label=f"Mult: {m}",
-        )
-    plt.legend()
-    plt.figure()
-    mults = dataframe["Mult"].unique()
-    for i, m in enumerate(mults):
-        data_previous = dataframe[dataframe["Pos"] == -1]
-
-        data_previous = data_previous[data_previous["Mult"] == m]
-        plt.plot(
-            data_previous["Layer"],
-            data_previous["Not Matching"],
-            color=colors[i],
-            label=f"Mult: {m}",
-        )
-    plt.legend()
-
-    plt.figure()
-    mults = dataframe["Mult"].unique()
-    for i, m in enumerate(mults):
-        data_previous = dataframe[dataframe["Pos"] == -1]
-        if abs(m) > 0.5:
-            continue
-
-        data_previous = data_previous[data_previous["Mult"] == m]
-        plt.plot(
-            data_previous["Layer"],
-            data_previous["Not Matching"] + data_previous["Matching"],
-            color=colors[i],
-            label=f"Mult: {m}",
-        )
-    plt.legend()
-
-    plt.figure()
-
-    mults = dataframe["Mult"].unique()
-    for i, m in enumerate(mults):
-        data_previous = dataframe[dataframe["Pos"] == -1]
-        if abs(m) > 0.5:
-            continue
-        data_previous = data_previous[data_previous["Mult"] == m]
-        plt.plot(
-            data_previous["Layer"],
-            data_previous["Average"],
-            color=colors[i],
-            label=f"Mult: {m}",
-        )
-    plt.legend()
-
-    mults = dataframe["Mult"].unique()
-    for i, m in enumerate(mults):
-        data_previous = dataframe[dataframe["Pos"] == -1]
-        if abs(m) > 0.5:
-            continue
-        data_previous = data_previous[data_previous["Mult"] == m]
-        plt.plot(
-            data_previous["Layer"],
-            data_previous["Correct"],
-            color=colors[i],
-            label=f"Mult: {m}",
-        )
-    plt.legend()
+    path = Path("results") / args.model / f"{args.behavior}.csv"
+    path.parent.mkdir(parents=True, exist_ok=True)
+    stats.to_csv(path)
